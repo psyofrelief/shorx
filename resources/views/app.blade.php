bc82474e485c2083c7b3835f2e02498b4dcc73b3
psyofrelief <6faried@gmail.com>
2024-08-26 14:35:24 +0800

    Add meta description to app view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Link shortening website">
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com">
